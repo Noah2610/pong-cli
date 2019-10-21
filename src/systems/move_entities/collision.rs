197be99a2127b327de6f4828a0a3c82f08db8345
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-10-22 01:28:39 +0200

    Split collision code from MoveEntitiesSystem into new module systems::move_entities::collision

---
+++ src/systems/move_entities/collision.rs
@@ -0,0 +1,204 @@
+//! Most of this module is copied from my deathframe crate.
+
+use super::*;
+
+/// Just a plain `Axis` enum with `X` and `Y` variants.
+#[derive(Clone, Debug, PartialEq)]
+pub(super) enum Axis {
+    X,
+    Y,
+}
+
+impl Axis {
+    /// This iterator-like method simply executes the passed closure with
+    /// the `X` variant and then for the `Y` variant.
+    pub fn for_each<C>(mut iterate: C)
+    where
+        C: FnMut(Self),
+    {
+        iterate(Axis::X);
+        iterate(Axis::Y);
+    }
+
+    /// Returns `true` if this is the `X` variant.
+    pub fn is_x(&self) -> bool {
+        Axis::X == *self
+    }
+
+    /// Returns `true` if this is the `Y` variant.
+    pub fn is_y(&self) -> bool {
+        Axis::Y == *self
+    }
+}
+
+#[derive(Default)]
+pub(super) struct CollisionGrid {
+    rects: Vec<CollisionRect>,
+}
+
+impl CollisionGrid {
+    pub fn push(&mut self, rect: CollisionRect) {
+        self.rects.push(rect);
+    }
+
+    pub fn collisions_with(
+        &self,
+        target: &CollisionRect,
+    ) -> Vec<CollisionType> {
+        self.rects
+            .iter()
+            .filter_map(|rect| {
+                if rect.id != target.id
+                    && Self::do_rects_intersect(&rect.rect, &target.rect)
+                {
+                    Some(rect.collision_type.clone())
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+
+    /// Returns `true` if the two passed `Rect`s intersect with each other.
+    #[rustfmt::skip]
+    fn do_rects_intersect(rect_one: &Rect, rect_two: &Rect) -> bool {
+        (
+            (
+                   rect_one.left >= rect_two.left
+                && rect_one.left <  rect_two.right
+            ) || (
+                   rect_one.left  <= rect_two.left
+                && rect_one.right >  rect_two.left
+            )
+        ) && (
+            (
+                   rect_one.top >= rect_two.top
+                && rect_one.top <  rect_two.bottom
+            ) || (
+                   rect_one.top    <= rect_two.top
+                && rect_one.bottom >  rect_two.top
+            )
+        )
+    }
+}
+
+pub(super) struct CollisionRect {
+    pub id:             Index,
+    pub collision_type: CollisionType,
+    pub rect:           Rect,
+}
+
+pub(super) fn new_collision_rect_and_position(
+    id: Index,
+    collision_type: &CollisionType,
+    position: &Position,
+    size: &Size,
+    axis: &Axis,
+    step: f32,
+) -> (CollisionRect, (f32, f32)) {
+    // Calculate new position
+    let new_position = (
+        position.x + if axis.is_x() { step } else { 0.0 },
+        position.y + if axis.is_y() { step } else { 0.0 },
+    );
+    let half_size = (size.w * 0.5, size.h * 0.5);
+    (
+        CollisionRect {
+            id:             id,
+            collision_type: collision_type.clone(),
+            rect:           Rect {
+                top:    new_position.1 - half_size.1,
+                bottom: new_position.1 + half_size.1,
+                left:   new_position.0 - half_size.0,
+                right:  new_position.0 + half_size.0,
+            },
+        },
+        new_position,
+    )
+}
+
+pub(super) fn gen_collision_grid(
+    entities: &Entities,
+    positions: &WriteStorage<Position>,
+    sizes: &ReadStorage<Size>,
+    collisions: &ReadStorage<Collision>,
+) -> CollisionGrid {
+    let mut collision_grid = CollisionGrid::default();
+    for (entity, position, size, collision) in
+        (entities, positions, sizes, collisions).join()
+    {
+        let half_size = (size.w * 0.5, size.h * 0.5);
+        collision_grid.push(CollisionRect {
+            id:             entity.id(),
+            collision_type: collision.collision_type.clone(),
+            rect:           Rect {
+                top:    position.y - half_size.1,
+                bottom: position.y + half_size.1,
+                left:   position.x - half_size.0,
+                right:  position.x + half_size.0,
+            },
+        });
+    }
+    collision_grid
+}
+
+pub(super) fn run_with_collision(
+    dt: f32,
+    collision_grid: &CollisionGrid,
+    entity: &Entity,
+    position: &mut Position,
+    velocity: &Velocity,
+    size: &Size,
+    collider: &mut Collider,
+    collision: &Collision,
+) {
+    let entity_id = entity.id();
+    collider.clear();
+
+    Axis::for_each(|axis| {
+        let vel = match axis {
+            Axis::X => velocity.x * dt,
+            Axis::Y => velocity.y * dt,
+        };
+        let abs = vel.abs() as usize;
+        let sign = if vel != 0.0 { vel.signum() } else { 0.0 };
+        let rem = vel % 1.0;
+
+        // Try to move by one absolute unit
+        for _ in 0 .. abs {
+            let (collision_rect, new_position) =
+                new_collision_rect_and_position(
+                    entity_id,
+                    &collision.collision_type,
+                    position,
+                    size,
+                    &axis,
+                    sign,
+                );
+            // Check for collision in newly calculated position
+            for collision_type in
+                collision_grid.collisions_with(&collision_rect)
+            {
+                collider.push(collision_type);
+            }
+            position.x = new_position.0;
+            position.y = new_position.1;
+        }
+        // Try to move by the floating point remainder
+        // Calculate new position
+        let (collision_rect, new_position) = new_collision_rect_and_position(
+            entity_id,
+            &collision.collision_type,
+            position,
+            size,
+            &axis,
+            rem,
+        );
+        // Check for collision in newly calculated position
+        for collision_type in collision_grid.collisions_with(&collision_rect) {
+            collider.push(collision_type);
+        }
+        position.x = new_position.0;
+        position.y = new_position.1;
+    });
+}
---
+++ src/systems/move_entities/mod.rs
@@ -0,0 +1,75 @@
+// TODO: Clean this mess up (collision stuff).
+
+mod collision;
+
+use super::system_prelude::*;
+use collision::*;
+
+#[derive(Default)]
+pub struct MoveEntitiesSystem;
+
+impl<'a> System<'a> for MoveEntitiesSystem {
+    type SystemData = (
+        Entities<'a>,
+        Read<'a, Deltatime>,
+        ReadStorage<'a, Size>,
+        ReadStorage<'a, Velocity>,
+        ReadStorage<'a, Collision>,
+        WriteStorage<'a, Position>,
+        WriteStorage<'a, Collider>,
+    );
+
+    fn run(
+        &mut self,
+        (
+            entities,
+            deltatime,
+            sizes,
+            velocities,
+            collisions,
+            mut positions,
+            mut colliders,
+        ): Self::SystemData,
+    ) {
+        let dt = deltatime.delta_seconds();
+
+        let collision_grid =
+            gen_collision_grid(&entities, &positions, &sizes, &collisions);
+
+        for (
+            entity,
+            mut position,
+            velocity,
+            size_opt,
+            collider_opt,
+            collision_opt,
+        ) in (
+            &entities,
+            &mut positions,
+            &velocities,
+            sizes.maybe(),
+            (&mut colliders).maybe(),
+            collisions.maybe(),
+        )
+            .join()
+        {
+            if let (Some(size), Some(mut collider), Some(collision)) =
+                (size_opt, collider_opt, collision_opt)
+            {
+                run_with_collision(
+                    dt,
+                    &collision_grid,
+                    &entity,
+                    &mut position,
+                    &velocity,
+                    &size,
+                    &mut collider,
+                    &collision,
+                );
+            } else {
+                position.x += velocity.x * dt;
+                position.y += velocity.y * dt;
+            }
+        }
+    }
+}
