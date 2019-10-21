b8d0b27660e1047753870ab58a9943fda7544c90
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-10-21 17:28:51 +0200

    Improve bin/share cargo_cmd function, always make cargo output with color

--- bin/share.sh
+++ bin/share.sh
@@ -4,9 +4,6 @@ _dir="$( dirname "$0" )"
 source "${_dir}/util.sh"
 unset _dir
 
-# https://stackoverflow.com/a/17841619/10927893
-function join_by { local IFS="$1"; shift; echo "$*"; }
-
 function cargo_cmd {
     local cargo_subcmd="$1"
     [ -z "$cargo_subcmd" ] && err "No cargo subcommand passed to function \`cargo_cmd\`"
@@ -14,18 +11,27 @@ function cargo_cmd {
     check "cargo"
     local args=("$@")
     unset 'args[0]'
-    local features_str
     local features=("$RUN_FEATURES")
-    features_str="$( join_by ',' "${features[@]}" )"
+    local features_str
+    features_str="${features[*]}"
     [ -n "$features_str" ] && features_str="--features $features_str"
-    local cmd="cargo +$RUST_VERSION $cargo_subcmd $features_str ${args[*]} && read"
+    local cmd=(
+        "cargo"
+        "+$RUST_VERSION"
+        "--color always"
+        "$cargo_subcmd"
+        "$features_str"
+        "${args[@]}"
+        ";"
+        "read"
+    )
     local run_msg
-    run_msg="$( colored "$COLOR_MSG_STRONG" "RUNNING:" ) $( colored "$COLOR_CODE" "$cmd" )"
+    run_msg="$( colored "$COLOR_MSG_STRONG" "RUNNING:" ) $( colored "$COLOR_CODE" "${cmd[*]}" )"
     echo -e "$run_msg"
     if should_run_in_terminal; then
-        run_terminal "$cmd"
+        run_terminal "${cmd[*]}"
     else
-        $cmd
+        eval "${cmd[*]}"
     fi
 }
 
